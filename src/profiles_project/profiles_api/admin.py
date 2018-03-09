f755a9fab0b0b42dc152f5b1aa3c931a4de60248
Sam Benny <ms_manivannan@yahoo.com>
2018-03-10 00:26:00 +0530

    Enabled the Django admin for the user profile mode

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
+
+from . import models
 # Register your models here.
+
+admin.site.register(models.UserProfile)
--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -50,7 +50,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD='email'
-    REQUIRED_FILEDS=['name']
+    REQUIRED_FIELDS=['name']
 
     def get_full_name(self):
         """ used to get a user full name """
